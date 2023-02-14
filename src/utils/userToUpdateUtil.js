27c28e9de1cb3ecdc956d951a1c3b457cd6aa6be
GiuGobbi <giuliagobbi1995@gmail.com>
2023-02-14 11:52:42 -0300

    User functions

--- src/utils/userToUpdateUtil.js
+++ src/utils/userToUpdateUtil.js
@@ -1,7 +1,5 @@
-const userToUpdateUtil = (userToUpdate, numDocumento, email, password, nombre, direccion, telephone) => {
+const userToUpdateUtil = (email, password, nombre, direccion, telephone) => {
     const changedDetail = {}
-    changedDetail.id = userToUpdate.dataValues.id
-    changedDetail.numDocumento = numDocumento
     changedDetail.email = email
     changedDetail.password = password
     changedDetail.nombre = nombre
