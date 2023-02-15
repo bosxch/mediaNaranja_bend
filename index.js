17bc13a24c62a1edecb622470ad94969dcc061f8
GiuGobbi <108301576+GiuGobbi@users.noreply.github.com>
2023-02-15 12:44:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const {PORT} = process.env;
 
 conn.sync({ alter: true }).then(() => {
-  server.listen(${PORT}, () => {
+  server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
