30df9dc2f5b42def8344656ec2ff6898f23c1d0b
bosxch <amparocardosobosch@gmail.com>
2023-02-16 20:44:08 -0300

    router use /store

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const user = require('./user');
 const tickets = require('./tickets');
+const store = require('./store')
 
 router.use('/user', user);
 router.use('/tickets', tickets);
