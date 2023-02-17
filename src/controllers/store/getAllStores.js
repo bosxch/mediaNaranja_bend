722bf121127d8f64c5a739b656dc6f0555cc0f81
GiuGobbi <giuliagobbi1995@gmail.com>
2023-02-16 23:13:38 -0300

    adde allStores util to store controllers

--- src/controllers/store/getAllStores.js
+++ src/controllers/store/getAllStores.js
@@ -1,8 +1,9 @@
 const {Store} = require('../../db')
+const { allStores } = require("../../utils/allStoresUtil")
 
 const getAllStores = async (req, res) => {
     try {
-        const stores = await Store.findAll()
+        const stores = await allStores()
         res.status(200).json(stores)
     } catch (error) {
         res.status(400).json(console.log(error))
--- src/controllers/store/getStoreByName.js
+++ src/controllers/store/getStoreByName.js
@@ -1,11 +1,12 @@
 const { Store } = require('../../db');
+const { allStores } = require("../../utils/allStoresUtil")
 
 const getStoreByName = async (req , res ) => {
 
     const {name} = req.query
     
     try {
-        const stores = await Store.findAll()
+        const stores = await allStores()
         const storeQuery = stores.filter(s => s.name.toLowerCase().includes(name.toLowerCase()))
         if (storeQuery) return res.status(200).json(storeQuery)
         else return res.status(400).json('No se encontraron almacenes con este nombre')
