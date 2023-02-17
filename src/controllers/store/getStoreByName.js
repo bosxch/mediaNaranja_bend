577e5cfe64b1f6ecaed9ddc0d92e4688a87dfb27
GiuGobbi <giuliagobbi1995@gmail.com>
2023-02-16 23:26:39 -0300

    changed controllers order in get store route

--- src/controllers/store/getStoreByName.js
+++ src/controllers/store/getStoreByName.js
@@ -1,20 +1,22 @@
 const { Store } = require('../../db');
 const { allStores } = require("../../utils/allStoresUtil")
 
-const getStoreByName = async (req , res ) => {
+const getStoreByName = async (req , res, next ) => {
 
     const {name} = req.query
-    
-    try {
-        const stores = await allStores()
-        const storeQuery = stores.filter(s => s.name.toLowerCase().includes(name.toLowerCase()))
-        if (storeQuery) return res.status(200).json(storeQuery)
-        else return res.status(400).json('No se encontraron almacenes con este nombre')
+    if (name) {
+        try {
+            const stores = await allStores()
+            const storeQuery = stores.filter(s => s.name.toLowerCase().includes(name.toLowerCase()))
+            if (storeQuery) return res.status(200).json(storeQuery)
+            else return res.status(400).json('No se encontraron almacenes con este nombre')
+        }
+        catch(error) {
+            return res.status(400).json(console.log(error))
+        };
+    } else {
+        next()
     }
-    catch(error) {
-        return res.status(400).json(console.log(error))
-    };
-
 };
 
 module.exports = {
