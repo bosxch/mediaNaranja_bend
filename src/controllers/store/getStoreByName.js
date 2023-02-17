c3ad544eff1c81b6081c9fe51095d6cd35ce0624
bosxch <amparocardosobosch@gmail.com>
2023-02-16 21:05:45 -0300

    getStoreByName route completed

---
+++ src/controllers/store/getStoreByName.js
@@ -0,0 +1,21 @@
+const { Store } = require('../../db');
+
+const getStoreByName = async (req , res ) => {
+
+    const {name} = req.query
+    
+    try {
+        const stores = await Store.findAll()
+        const storeQuery = stores.filter(s => s.name.toLowerCase().includes(name.toLowerCase()))
+        if (storeQuery) return res.status(200).json(storeQuery)
+        else return res.status(400).json('No se encontraron almacenes con este nombre')
+    }
+    catch(error) {
+        return res.status(400).json(console.log(error))
+    };
+
+};
+
+module.exports = {
+    getStoreByName
+}
