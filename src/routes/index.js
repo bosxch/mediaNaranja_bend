06cac55b2a482ad45372a56327a79b5b1375370c
bosxch <amparocardosobosch@gmail.com>
2023-02-18 15:56:38 -0300

    declared admin path

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 const router = Router();
 const user = require('./user');
 const tickets = require('./tickets');
-const store = require('./store')
+const store = require('./store');
+const admin = require('./admin');
 
 router.use('/user', user);
 router.use('/tickets', tickets);
-router.use('/store', store)
+router.use('/store', store);
+router.use('/admin', admin);
 
 module.exports = router;
