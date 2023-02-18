630e4aa66541d054cffd0e41e1ef6a986bbe0bf5
bosxch <amparocardosobosch@gmail.com>
2023-02-18 16:26:28 -0300

    working on route put password

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const { getDataAdmin } = require("../controllers/admin/getDataAdmin.js");
-const { postPasword } = require("../controllers/admin/postPasword.js");
+const { postAdmin } = require("../controllers/admin/postAdmin.js");
+const {putPassword} = require("../controllers/admin/putPassword.js")
 
 const adminRouer = Router();
 
 adminRouer.get("/", getDataAdmin);
-adminRouer.post("/", postPasword);
+adminRouer.post("/", postAdmin);
+adminRouer.put('/', putPassword);
 
 
 module.exports = adminRouer
---
+++ src/utils/AdminUpdatePassword.js
@@ -0,0 +1,10 @@
+const AdminToUpdatePassword = ( password ) => {
+    const changedPassword = {}
+    changedPassword.password = password
+
+    return changedPassword
+}
+
+module.exports = {
+    AdminToUpdatePassword
+}
