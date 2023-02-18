d118aa4f60054a44cda3b5fdcb0a8ad8d3bab13f
bosxch <amparocardosobosch@gmail.com>
2023-02-18 15:50:27 -0300

    admin model created

---
+++ src/models/Admin.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+          },
+          email: {
+            type: DataTypes.STRING,
+            unique: true,
+            validate: {
+                isEmail: true
+            },
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        countdown: {
+            type: DataTypes.smalldatetime,
+        },
+    },
+    {
+        freezeTableName: true,
+        timestamps: false,
+        createdAt: false,
+        updatedAt: false
+    })
+}
