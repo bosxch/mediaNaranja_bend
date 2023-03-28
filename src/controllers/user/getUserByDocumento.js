2ae5d2904a57be39a68484627a5dfb7014d88ea1
bosxch <amparocardosobosch@gmail.com>
2023-03-28 13:11:33 -0300

    getUserByDocumento with JWT

--- src/controllers/user/getUserByDocumento.js
+++ src/controllers/user/getUserByDocumento.js
@@ -1,4 +1,6 @@
 const { allUsers } = require("../../utils/allUserUtil")
+const jwt = require('jsonwebtoken')
+const {jwTokens} = require('../../utils/jwtUtil')
 
 const getByDocumento = async (req, res, next) => {
 
@@ -8,11 +10,15 @@ const getByDocumento = async (req, res, next) => {
             try{
             const usersDB =  await allUsers() 
             const results = usersDB.find((e) => e.numDocumento == numDocumento)
-            if (results) return res.status(200).json(results)
+            if (results) {
+            let tokens = jwTokens(results.id)
+            res.cookie('refresh_token',tokens.refreshToken,{httpOnly})
+            return res.status(200).json(results, tokens)
+            }
             else return res.status(404).json("No hay usuarios con ese documento")
             }
             catch(error) { 
-            return res.status(400).json(console.log(error))
+            return res.status(400).json(console.log({error: error.message}))
             } 
         }else{
             next()
